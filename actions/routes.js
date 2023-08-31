416e4142710d1c4d1a7846852668d8ea83b3aee8
petermc87 <peter.mcgibney@gmail.com>
2023-08-31 18:49:02 -0400

    fix: remove backend service

--- actions/routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { dataController, apiController } = require("./dataController");
-
-// Show
-router.get("/", dataController.getSearched, apiController.getSearched);
-
-module.exports = router;
